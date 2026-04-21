b7a7695809717ef4ce348a0f59cff83f15f39b7a
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-04-21 18:53:23 -0500

    init project

---
+++ queue_management/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace queue_management.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
